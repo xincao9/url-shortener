95afbe73adc384e18563e7ff38844366eb694e53
xincao9 <xincao9@gmail.com>
2025-01-06 23:28:06 +0800

    添加b62编码

--- routes/urls.js
+++ routes/urls.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 const Url = require('../models/url')
 const { runConfig } = require('../config/config')
+const { encode, decode } = require('../utils/base62')
 
 router.post('/', async (req, res) => {
   const { raw } = req.body
@@ -12,7 +13,8 @@ router.post('/', async (req, res) => {
   try {
     const url = await Url.create({ raw })
     const domain = runConfig.domain
-    res.json({ s: `${domain}/urls/${url.id}` })
+    const sid = encode(url.id)
+    res.json({ s: `${domain}/urls/${sid}` })
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
@@ -21,7 +23,8 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   let { id } = req.params
   try {
-    const url = await Url.findOne({ where: { id } })
+    const nid = decode(id)
+    const url = await Url.findOne({ where: { id: nid } })
     if (url === null) {
       res.status(400).json({ error: 'Not found!' })
       return
