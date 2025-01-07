3aa00cf14bb923713ff9a959860721c3bf16e2c8
xincao9 <xincao9@gmail.com>
2025-01-07 19:23:56 +0800

    修改历史记录持久化

--- front/src/stores/create-history.js
+++ front/src/stores/create-history.js
@@ -1,12 +1,21 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCreateHistoryStore = defineStore('create-history', () => {
-  const createHistory = ref([])
+export const useCreateHistoryStore = defineStore(
+  'create-history',
+  () => {
+    const createHistory = ref([])
 
-  function add({ raw, s }) {
-    createHistory.value.unshift({ raw, s, createTime: new Date().toISOString() })
-  }
+    function add({ raw, s }) {
+      createHistory.value.unshift({ raw, s, createTime: new Date().toISOString() })
+    }
 
-  return { createHistory, add }
-})
+    return { createHistory, add }
+  },
+  {
+    persist: {
+      storage: sessionStorage,
+      pick: ['createHistory'],
+    },
+  },
+)
