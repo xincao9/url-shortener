0e5bc6063a54a888f7f2ba38d53b89a04cdf8e6f
xincao9 <xincao9@gmail.com>
2025-01-07 21:52:59 +0800

    OK

---
+++ plugins/sequelize.js
@@ -0,0 +1,16 @@
+const { RunConfig } = require('../config/config')
+
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(RunConfig.mysql)
+
+sequelize
+  .authenticate()
+  .then((value) => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error)
+  })
+
+module.exports = sequelize
