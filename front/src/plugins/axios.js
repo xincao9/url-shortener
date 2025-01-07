63ba36ec23ecf032ebbc222001934589b1351b06
xincao9 <xincao9@gmail.com>
2025-01-07 18:16:11 +0800

    OK

---
+++ front/src/plugins/axios.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 1000,
+  headers: { 'Content-Type': 'application/json' },
+})
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // const token = localStorage.getItem('token');
+    // if (token) {
+    //     config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+export default axiosInstance
--- front/src/views/UrlCreateView.vue
+++ front/src/views/UrlCreateView.vue
@@ -1,27 +1,47 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import axios from '../plugins/axios'
+
+const statistics = ref({
+  visits: 0,
+  usage: 0,
+  success: 0,
+  failures: 0,
+})
+const raw = ref('')
+
+const create = async () => {
+  try {
+    const response = await axios.post('/urls', { raw: raw.value })
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
 
 <template>
   <el-container>
     <el-header>
       <el-row style="text-align: center; margin-top: 50px">
         <el-col :span="6">
-          <el-statistic title="访问量" :value="1" />
+          <el-statistic title="访问量" :value="statistics.visits" />
         </el-col>
         <el-col :span="6">
-          <el-statistic title="使用量" :value="2" />
+          <el-statistic title="使用量" :value="statistics.usage" />
         </el-col>
         <el-col :span="6">
-          <el-statistic title="成功量" :value="3" />
+          <el-statistic title="成功量" :value="statistics.success" />
         </el-col>
         <el-col :span="6">
-          <el-statistic title="失败量" :value="4" />
+          <el-statistic title="失败量" :value="statistics.failures" />
         </el-col>
       </el-row>
     </el-header>
     <el-main>
       <el-row style="text-align: center; margin-top: 50px">
         <el-col :span="8" :offset="8">
-          <el-image style="width: 100px; height: 100px" :src="url" :fit="fit" />
+          <el-image style="width: 100px; height: 100px" src="url" :fit="fit" />
         </el-col>
       </el-row>
       <el-row style="margin-top: 20px">
@@ -31,7 +51,7 @@
               <el-input v-model="raw" />
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" style="margin: auto">新建</el-button>
+              <el-button type="primary" style="margin: auto" @click="create">新建</el-button>
             </el-form-item>
           </el-form>
         </el-col>
