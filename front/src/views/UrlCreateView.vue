ad6673a05145bc461eb4e8e6f499c8b6de6e24ff
xincao9 <xincao9@gmail.com>
2025-01-07 16:33:53 +0800

    添加创建页面

--- front/src/views/UrlCreateView.vue
+++ front/src/views/UrlCreateView.vue
@@ -1,11 +1,30 @@
 <script setup></script>
 
 <template>
-  <el-container style="height: 500px">
+  <el-container>
     <el-header>
+      <el-row style="text-align: center; margin-top: 50px;">
+        <el-col :span="6">
+          <el-statistic title="访问量" :value="1" />
+        </el-col>
+        <el-col :span="6">
+          <el-statistic title="使用量" :value="2" />
+        </el-col>
+        <el-col :span="6">
+          <el-statistic title="成功量" :value="3" />
+        </el-col>
+        <el-col :span="6">
+          <el-statistic title="失败量" :value="4" />
+        </el-col>
+      </el-row>
     </el-header>
     <el-main>
-      <el-row>
+      <el-row style="text-align: center; margin-top: 50px;">
+        <el-col :span="8" :offset="8">
+          <el-image style="width: 100px; height: 100px;" :src="url" :fit="fit" />
+        </el-col>
+      </el-row>
+      <el-row style="margin-top: 20px;">
         <el-col :span="8" :offset="8">
           <el-form>
             <el-form-item label="链接">
