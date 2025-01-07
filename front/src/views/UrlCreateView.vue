7dd9d6e9e01f73e517818cb6f9c1125b124158e2
xincao9 <xincao9@gmail.com>
2025-01-07 16:17:35 +0800

    OK

--- front/src/views/UrlCreateView.vue
+++ front/src/views/UrlCreateView.vue
@@ -1,10 +1,24 @@
 <script setup></script>
 
 <template>
-  <el-container>
-    <el-header height="100px" style="border: 2px solid red"></el-header>
-    <el-main style="border: 2px solid blue"></el-main>
-    <el-footer height="100px" style="border: 2px solid green"></el-footer>
+  <el-container style="height: 500px">
+    <el-header>
+    </el-header>
+    <el-main>
+      <el-row>
+        <el-col :span="8" :offset="8">
+          <el-form>
+            <el-form-item label="链接">
+              <el-input v-model="raw" />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" style="margin: auto;">新建</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer></el-footer>
   </el-container>
 </template>
 
