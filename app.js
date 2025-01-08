18d5f54fe3908648cf77ea0f2c255294dc16b3e9
xincao9 <xincao9@gmail.com>
2025-01-08 23:41:14 +0800

    OK

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
@@ -26,21 +25,5 @@ app.use(express.static(path.join(__dirname, 'front/dist')))
 app.use('/users', usersRouter)
 app.use('/urls', urlsRouter)
 app.use('/statistics', statisticsRouter)
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
 
 module.exports = app
