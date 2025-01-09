4df290638e9aa5e5037f0b592fb6f4333eed36dd
xincao9 <xincao9@gmail.com>
2025-01-09 15:53:57 +0800

    短链格式优化

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express')
 var router = express.Router()
+const { decode } = require('../utils/base62')
+const Url = require('../models/url')
+const { increment, keys } = require('../models/statistics')
 
-// /* GET home page. */
-// router.get('/', function (req, res, next) {
-//   res.render('index', { title: 'url-shortener' })
-// })
+router.get('/:id', async (req, res) => {
+  let { id } = req.params
+  try {
+    const nid = decode(id)
+    const url = await Url.findOne({ where: { id: nid } })
+    if (url === null) {
+      res.status(400).json({ error: 'Not found!' })
+      return
+    }
+    res.redirect(url.raw)
+    increment(keys.usage)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+    increment(keys.failures)
+  }
+})
 
 module.exports = router
--- routes/urls.js
+++ routes/urls.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 const Url = require('../models/url')
 const { RunConfig } = require('../config/config')
-const { encode, decode } = require('../utils/base62')
+const { encode } = require('../utils/base62')
 const { increment, keys } = require('../models/statistics')
 
 router.post('/', async (req, res) => {
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
     }
     const domain = RunConfig.domain
     const sid = encode(url.id)
-    res.json({ s: `${domain}/urls/${sid}` })
+    res.json({ s: `${domain}/s/${sid}` })
     increment(keys.success)
   } catch (err) {
     res.status(500).json({ error: err.message })
@@ -26,20 +26,4 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res) => {
-  let { id } = req.params
-  try {
-    const nid = decode(id)
-    const url = await Url.findOne({ where: { id: nid } })
-    if (url === null) {
-      res.status(400).json({ error: 'Not found!' })
-      return
-    }
-    res.redirect(url.raw)
-    increment(keys.usage)
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-    increment(keys.failures)
-  }
-})
 module.exports = router
