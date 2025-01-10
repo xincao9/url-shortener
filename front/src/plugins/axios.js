46c14cd7445cb0d674bf1a4255d93dcdd4feab94
xincao9 <xincao9@gmail.com>
2025-01-11 00:03:52 +0800

    修改时间

--- front/src/plugins/axios.js
+++ front/src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000',
-  timeout: 1000,
+  timeout: 3000,
   headers: { 'Content-Type': 'application/json' },
 })
 
