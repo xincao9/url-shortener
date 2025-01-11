49c24f465432811ab415a141fc2483b348fb1edb
xincao9 <xincao9@gmail.com>
2025-01-11 22:43:36 +0800

    OK

--- front/src/components/FooterComponent.vue
+++ front/src/components/FooterComponent.vue
@@ -4,12 +4,14 @@
   <el-container>
     <el-footer>
       <el-row>
-        <el-col :span="24" style="background-color: aliceblue">
-          <el-divider />
+        <el-col :span="24" style="background-color: aliceblue; text-align: center; height: 100px">
+          <div style="margin: 30px 0px auto">Released under the MIT License.</div>
+          <div>
+            Made with ❤️ by <a href="https://github.com/xincao9/" target="_blank">xincao9</a>
+          </div>
         </el-col>
       </el-row>
     </el-footer>
   </el-container>
 </template>
-
 <style scoped></style>
