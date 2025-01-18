c8dba5bffa7c4fad58dfccf53f140f884b969bc8
xincao9 <xincao9@gmail.com>
2025-01-18 21:01:11 +0800

    OK

--- front/src/plugins/axios.js
+++ front/src/plugins/axios.js
@@ -8,10 +8,10 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    // const token = localStorage.getItem('token');
-    // if (token) {
-    //     config.headers.Authorization = `Bearer ${token}`;
-    // }
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
     return config
   },
   (error) => {
