4ec4514e1aa4d972650d5803bf3d7c1d95338e8c
xincao9 <xincao9@gmail.com>
2025-02-04 17:24:37 +0800

    少提交代码

---
+++ front/src/components/MavenProjectCreate.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-dialog
+    v-model="props.show"
+    title="🦏 创建项目"
+    destroy-on-close
+    center
+    align-center
+    @close="close"
+    width="50%"
+  >
+    <el-form :model="form" :rules="rules" ref="formRef" label-width="auto">
+      <el-form-item prop="groupId">
+        <el-input v-model="form.groupId" placeholder="com.github.xincao9">
+          <template #prepend>🐰 组ID</template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="artifactId">
+        <el-input v-model="form.artifactId" placeholder="demo">
+          <template #prepend>🐁 件ID</template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="version">
+        <el-input v-model="form.version" placeholder="1.0.0-SNAPSHOT">
+          <template #prepend>🦇 版本</template>
+        </el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button type="success" round @click="submit">🐭 提交</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import { defineEmits, ref } from 'vue'
+import axios from '../plugins/axios'
+
+const emit = defineEmits(['show:closed'])
+const props = defineProps({
+  show: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+})
+
+const close = () => {
+  emit('show:closed', true)
+}
+
+const form = ref({
+  groupId: '',
+  artifactId: '',
+  version: '',
+})
+
+const formRef = ref(null)
+
+const rules = {
+  groupId: [{ required: true, message: '请输入组ID', trigger: 'blur' }],
+  artifactId: [{ required: true, message: '请输入工件ID', trigger: 'blur' }],
+  version: [{ required: true, message: '请输入版本号', trigger: 'blur' }],
+}
+
+const submit = async () => {
+  const formRefValue = formRef.value
+  formRefValue.validate((valid) => {
+    if (valid) {
+      try {
+        console.log(form.value)
+        const { id } = axios.post('/maven/generate', form.value)
+        if (id) {
+          downloadFile(`/maven/download/${id}`, form.value.artifactId)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      return false
+    }
+  })
+}
+
+function downloadFile(url, artifactId) {
+  const a = document.createElement('a')
+  a.href = url
+  a.download = `${artifactId}.zip`
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+}
+</script>
+
+<style scoped></style>
