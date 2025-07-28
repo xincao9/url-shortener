09b8ac8cfe34e34d94958a4edc2c6d5905455653
fochat666 <fochat666@gmail.com>
2025-07-28 15:26:51 +0800

    去掉恶心de

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -6,7 +6,6 @@
       <el-col :span="24" style="text-align: center">
         <div>Released under the MIT License.</div>
         <div>Made with ❤️ by <a href="https://github.com/xincao9/" target="_blank">xincao9</a></div>
-        <p>IP地址位置数据由<a href="https://www.cz88.net">纯真CZ88</a>提供支持</p>
       </el-col>
     </el-row>
   </el-footer>
